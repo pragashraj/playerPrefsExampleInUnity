9870c2894bfac1351980f147b1d966b6be7a184d
S.Pragashraj <rajpragashraj30@gmail.com>
2020-03-04 07:28:15 +0530

    simple Example

---
+++ Assets/RollScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RollScript : MonoBehaviour {
+
+	public Text score;
+	public Text heighestScoreText;
+
+	void Start(){
+		heighestScoreText.GetComponent<Text>().text=PlayerPrefs.GetInt("HeighestScore",0).ToString();
+	}
+
+	public void rollDice(){
+		int rollNumber=Random.Range(1,7);
+		score.GetComponent<Text>().text=rollNumber.ToString();
+		int highScore=PlayerPrefs.GetInt("HeighestScore");
+		if(highScore<rollNumber){
+			PlayerPrefs.SetInt("HeighestScore",rollNumber);
+		}
+		heighestScoreText.GetComponent<Text>().text=PlayerPrefs.GetInt("HeighestScore",0).ToString();
+		
+	}
+
+	public void resetScore(){
+		score.GetComponent<Text>().text="0";
+		heighestScoreText.GetComponent<Text>().text="0";
+		PlayerPrefs.SetInt("HeighestScore",0);
+	}
+}
